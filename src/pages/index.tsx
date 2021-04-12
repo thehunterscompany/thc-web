ac725ed36bf4282f1edcd991e3c068a2635a9767
Armando Zubillaga <ajzpiv97@gmail.com>
2021-04-12 13:28:36 -0400

    Added favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,6 +23,27 @@ export default function Home() {
         <link rel="stylesheet" href="css/bootstrap.min.css" />
         <link rel="stylesheet" href="css/main.css" />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+
         <script src="js/modernizr-2.7.1.js"></script>
       </Head>
 
