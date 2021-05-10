696b3d608c4eb8630786db4a955654e48898cb5f
Armando Zubillaga <ajzpiv97@gmail.com>
2021-05-10 13:28:43 -0400

    TW-1 Added function to load images

--- public/js/main.js
+++ public/js/main.js
@@ -1,45 +1,54 @@
-$(document).ready(function(){
-  
-  
+$(document).ready(function () {
   //------------------------------------//
   //Navbar//
   //------------------------------------//
-    	var menu = $('.navbar');
-    	$(window).bind('scroll', function(e){
-    		if($(window).scrollTop() > 140){
-    			if(!menu.hasClass('open')){
-    				menu.addClass('open');
-    			}
-    		}else{
-    			if(menu.hasClass('open')){
-    				menu.removeClass('open');
-    			}
-    		}
-    	});
-  
-  
+  var menu = $(".navbar");
+  $(window).bind("scroll", function (e) {
+    if ($(window).scrollTop() > 140) {
+      if (!menu.hasClass("open")) {
+        menu.addClass("open");
+      }
+    } else {
+      if (menu.hasClass("open")) {
+        menu.removeClass("open");
+      }
+    }
+  });
+
+  // Load Images
+  var imagesLoaded = 0;
+  // Total images is still the total number of <img> elements on the page.
+  var totalImages = $("img").length;
+
+  // Step through each image in the DOM, clone it, attach an onload event
+  // listener, then set its source to the source of the original image. When
+  // that new image has loaded, fire the imageLoaded() callback.
+  $("img").each(function (idx, img) {
+    $("<img>").on("load", imageLoaded).attr("src", $(img).attr("src"));
+  });
+
+  // Do exactly as we had before -- increment the loaded count and if all are
+  // loaded, call the allImagesLoaded() function.
+  function imageLoaded() {
+    imagesLoaded++;
+    if (imagesLoaded == totalImages) {
+      allImagesLoaded();
+    }
+  }
+
+  function allImagesLoaded() {
+    console.log("ALL IMAGES LOADED");
+  }
+
   //------------------------------------//
   //Scroll To//
   //------------------------------------//
-  $(".scroll").click(function(event){		
-  	event.preventDefault();
-  	$('html,body').animate({scrollTop:$(this.hash).offset().top}, 800);
-  	
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 800);
   });
-  
+
   //------------------------------------//
   //Wow Animation//
-  //------------------------------------// 
-  wow = new WOW(
-        {
-          boxClass:     'wow',      // animated element css class (default is wow)
-          animateClass: 'animated', // animation css class (default is animated)
-          offset:       0,          // distance to the element when triggering the animation (default is 0)
-          mobile:       false        // trigger animations on mobile devices (true is default)
-        }
-      );
-      wow.init();
-
-
-	
+  //------------------------------------//
 });
