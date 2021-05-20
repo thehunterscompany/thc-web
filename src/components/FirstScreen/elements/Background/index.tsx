cabcc7cb60ac9d0f2d87df60def451581c0355e4
Armando Zubillaga <ajzpiv97@gmail.com>
2021-05-20 12:28:13 -0400

    TW-10 Added global styles

--- src/components/FirstScreen/elements/Background/index.tsx
+++ src/components/FirstScreen/elements/Background/index.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import styles from "./styles.module.scss";
 
 const BackGround = () => (
-  <div className={styles["_background-space"]}>
-    <div
-      className={styles["_background-img-wrapper"]}
-      style={{ height: "750px" }}
-    >
+  <div className="_background-space">
+    <div className="_background-img-wrapper" style={{ height: "750px" }}>
       <img
-        className={styles["_background-img"]}
+        className="_background-img"
         src="/images/background/fondo-small.jpg"
         srcSet="/images/background/fondo-full-size.jpg 1800w"
       />
---
+++ src/components/FirstScreen/elements/Text/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./styles.module.scss";
+
+const Text = () => (
+  <div className="_text-space">
+    <h1 className="_text-space-title">Unlimited movies, TV shows, and more.</h1>
+    <h2 className="_text-space-subtitle">Watch anywhere. Cancel anytime.</h2>
+    <div>
+      <a className="_btn _btn-secondary _btn-lg">Calcula tu Cuota</a>
+    </div>
+  </div>
+);
+
+export default Text;
