c4ad76bd9d89c73d6c2e47bf7e30e8c39b4771ee
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-03 15:10:30 -0400

    TW-27 Added img to css and spacing for btn

--- src/components/FirstScreen/elements/Background/index.tsx
+++ src/components/FirstScreen/elements/Background/index.tsx
@@ -3,12 +3,9 @@ import styles from "./styles.module.scss";
 
 const BackGround = () => (
   <div className="_background-space">
-    <div className="_background-img-wrapper" style={{ height: "750px" }}>
-      <img
-        className="_background-img"
-        src="/images/background/fondo-small.jpg"
-        srcSet="/images/background/fondo-full-size.jpg 1800w"
-      />
+    <div className="_background-img-wrapper" style={{ height: "70rem" }}>
+      <div className="_background-img" />
+      {/* <div className="_background-img-gradient"></div> */}
     </div>
   </div>
 );
--- src/components/FirstScreen/elements/Text/index.tsx
+++ src/components/FirstScreen/elements/Text/index.tsx
@@ -7,7 +7,7 @@ const Text = () => (
       Encuentra la mejor financiación para tu vivienda
     </h1>
     <h2 className="_text-space-subtitle">Rápido y sin costo.</h2>
-    <div>
+    <div style={{ marginTop: "47px" }}>
       <a className="_btn _btn-secondary _btn-lg">Calcula tu Cuota</a>
     </div>
   </div>
