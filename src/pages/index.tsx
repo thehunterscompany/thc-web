79e5b60bf83cfd67614074148dc08c594d39627a
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-03 15:08:54 -0400

    TW-9 Added description and modified viewport

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ export default function Home() {
     <>
       <Head>
         <title>Te conseguimos la mejor financiación para tu vivienda</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
-          name="viewport"
-          content="initial-scale=1.0, width=device-width maximum-scale=5.0"
+          name="description"
+          content="Landing Page para la compañia The Hunters company. Es una empresa broker de servicios de financiación
+                  de vivienda e inmuebles comercial para personas naturales."
         />
       </Head>
       <Sections />
