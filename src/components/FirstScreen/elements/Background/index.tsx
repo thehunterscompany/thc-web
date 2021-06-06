daf7c8d9a22a23b557778ec204dabf88eb08a3b7
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-06 18:36:05 -0400

    TW-27 Modified position

--- src/components/FirstScreen/elements/Background/index.tsx
+++ src/components/FirstScreen/elements/Background/index.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 
 const BackGround = () => (
   <div className="_background-space">
-    <div className="_background-img-wrapper" style={{ height: "70rem" }}>
+    <div className="_background-img-wrapper" style={{ height: "76rem" }}>
       <div className="_background-img" />
       {/* <div className="_background-img-gradient"></div> */}
     </div>
