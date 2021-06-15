e93d0fffce9f75bfde325526084163d339ea62b6
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-15 13:06:37 -0500

    TW-29 Added grid and styles

---
+++ public/images/logos/bancos/bancosLogos.ts
@@ -0,0 +1,53 @@
+export type bancosLogosAttr = {
+  alt: string;
+  img: string;
+  href: string;
+};
+
+export const bancosLogos: Array<bancosLogosAttr> = [
+  {
+    alt: "BBVA",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.bbva.com.co/",
+  },
+  {
+    alt: "Scotiabank Colpatria",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.scotiabankcolpatria.com/",
+  },
+  {
+    alt: "MR Inversiones Inmobiliarias",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://mrinversionesinmobiliarias.com/",
+  },
+  {
+    alt: "Banco AV Villas",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.avvillas.com.co/wps/portal/avvillas/",
+  },
+  {
+    alt: "Casa Propia Colombia",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://casapropiacolombia.com/en",
+  },
+  {
+    alt: "Banco Caja Social",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.bancocajasocial.com/portalserver/bcs-public/inicio",
+  },
+  {
+    alt: "Banco de Occidente",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.bancodeoccidente.com.co/wps/portal/banco-de-occidente/bancodeoccidente/",
+  },
+  {
+    alt: "Itaú CorpBanca Colombia SA",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.itau.co/index",
+  },
+  {
+    alt: "Banco Popular Colombia",
+    img: "images/logos/bancos/CasaPropia.png",
+    href: "https://www.bancopopular.com.co",
+  },
+];
--- src/components/Bancos/Bancos.tsx
+++ src/components/Bancos/Bancos.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
 import styles from "./FirstScreen.module.scss";
-import NavBar from "../NavBar/NavBar";
 import Text from "./element/Text";
+import BancosGrid from "./element/BancosGrid";
 
 const Bancos = () => (
   <div className="_thc-story-section _thc-card">
     <Text />
+    <BancosGrid />
   </div>
 );
 
