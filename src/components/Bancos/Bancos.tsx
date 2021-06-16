7e0a5496ca321eabdf83af44c00eb2a882264cc3
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-15 22:02:30 -0500

    TW-32 Created list of banks

--- src/components/Bancos/Bancos.tsx
+++ src/components/Bancos/Bancos.tsx
@@ -2,11 +2,15 @@ import React, { Fragment } from "react";
 import styles from "./FirstScreen.module.scss";
 import Text from "./element/Text";
 import BancosGrid from "./element/BancosGrid";
+import GridSinFotos from "./element/BancosGridSinImg";
 
 const Bancos = () => (
-  <div className="_thc-story-section _thc-card">
+  <div
+    className="_thc-story-section _thc-card "
+    style={{ backgroundColor: "#aedaff" }}
+  >
     <Text />
-    <BancosGrid />
+    <GridSinFotos />
   </div>
 );
 
