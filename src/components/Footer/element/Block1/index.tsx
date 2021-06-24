4b31d768a0eaa40fe4ebd2b12c79b1c661dc273a
Armando Zubillaga <ajzpiv97@gmail.com>
2021-06-24 13:06:11 -0500

    Modified spacing, added nooppener tag and a label

--- src/components/Footer/element/Block1/index.tsx
+++ src/components/Footer/element/Block1/index.tsx
@@ -23,7 +23,9 @@ const blockList = [
     children: (
       <Fragment>
         <img src="images/footer/email.svg" alt="Email" />
-        <a href="mailto:info@thcsas.com.co">info@thcsas.com.co</a>
+        <a href="mailto:info@thcsas.com.co" rel="noopener noreferrer">
+          info@thcsas.com.co
+        </a>
       </Fragment>
     ),
   },
@@ -34,6 +36,7 @@ const blockList = [
         <a
           href="https://api.whatsapp.com/send?phone=+573104908414"
           target="_blank"
+          rel="noopener noreferrer"
         >
           310 490 8414
         </a>
@@ -44,7 +47,9 @@ const blockList = [
     children: (
       <Fragment>
         <img src="images/footer/phone.svg" alt="Phone" />
-        <a href="tel:+573104908414">311 854 2488</a>
+        <a href="tel:+573104908414" rel="noopener noreferrer">
+          311 854 2488
+        </a>
       </Fragment>
     ),
   },
@@ -56,6 +61,7 @@ const blockList = [
           <a
             href="http://maps.google.com/?q=Cra.+14+%2396-15,+Bogotá,+Colombia"
             target="_blank"
+            rel="noopener noreferrer"
           >
             Carrera 14#96-15 piso 1. Bogotá DC
           </a>
@@ -77,6 +83,8 @@ const blockList = [
           <a
             href="https://www.instagram.com/the_hunters_company/"
             target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Instagram"
           >
             <i className="fa fa-instagram"></i>
           </a>
@@ -85,6 +93,8 @@ const blockList = [
           <a
             href="https://www.linkedin.com/company/the-hunters-company-sas/"
             target="_blank"
+            rel="noopener noreferrer"
+            aria-label="Linkedin"
           >
             <i className="fa fa-linkedin"></i>
           </a>
