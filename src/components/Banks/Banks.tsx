2250d61ff0c0b1a8f14cef948b18a783ee665369
Armando Zubillaga <ajzpiv97@gmail.com>
2021-07-01 21:18:03 -0400

    TW-57 Fixed grid for banks

--- src/components/Banks/Banks.tsx
+++ src/components/Banks/Banks.tsx
@@ -4,9 +4,14 @@ import BancosGrid from "./element/BancosGrid";
 import GridSinFotos from "./element/BancosGridSinImg";
 
 const Banks = () => (
-  <div className="_thc-story-section _thc-card ">
+  <div
+    className="_thc-story-section _thc-card "
+    style={{ backgroundColor: "#f5f5f5" }}
+  >
     <Text />
-    <GridSinFotos />
+    <div className="_content-space">
+      <GridSinFotos />
+    </div>
   </div>
 );
 
