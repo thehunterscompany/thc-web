9710393368e3e9b630318a68fb4f3da980a3710c
Armando Zubillaga <ajzpiv97@gmail.com>
2021-07-15 11:13:33 -0400

    Refactoring components

--- src/components/FirstScreen/FirstScreen.tsx
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./FirstScreen.module.scss";
-import Background from "./elements/Background/index";
+import Background from "../Background/Background";
 import Text from "./elements/Text/index";
 
 const FirstScreen = () => (
   <div className={`_thc-story-section ${styles["_first-screen"]} _thc-card`}>
-    <Background />
+    <Background classNamePath={styles["_background-img-wrapper"]} />
     <Text />
   </div>
 );
